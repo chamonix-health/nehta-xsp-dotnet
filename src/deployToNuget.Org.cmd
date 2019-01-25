c20639c93a0a77499f1f857cf0e89615835ea24a
philip.wilford <philip.wilford@digitalhealth.gov.au>
2019-01-25 10:10:14 +1000

    Updated deploy script for next time

--- src/deployToNuget.Org.cmd
+++ src/deployToNuget.Org.cmd
@@ -2,7 +2,9 @@ del *.nupkg
 
 msbuild /p:Configuration=Release
 
-NuGet.exe pack Nehta.Xsp\Nehta.Xsp.csproj
+NuGet.exe pack Nehta.Xsp\Nehta.Xsp.csproj -Properties Configuration=Release
+
+pause
 
 forfiles /m *.nupkg /c "cmd /c NuGet.exe push @FILE -Source https://www.nuget.org/api/v2/package"
 
