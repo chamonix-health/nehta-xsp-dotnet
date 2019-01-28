f72683461f6818b3dc3881e78e9b77e19bad853d
philip.wilford <philip.wilford@digitalhealth.gov.au>
2019-01-29 09:34:08 +1000

    Added licence details + github reference

--- src/Nehta.Xsp/Properties/AssemblyInfo.cs
+++ src/Nehta.Xsp/Properties/AssemblyInfo.cs
@@ -32,8 +32,5 @@ using System;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.3.0.1")]
-[assembly: AssemblyFileVersion("1.3.0.1")]
-
-
-[assembly: CLSCompliant(true)]
+[assembly: AssemblyVersion("1.3.0.2")]
+[assembly: AssemblyFileVersion("1.3.0.2")]
